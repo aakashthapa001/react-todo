b191a2205aeff55e9b7bef9d9a6085d6c5bb4742
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2016-12-07 15:55:14 +0545

    Add localStorage support

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -1,17 +1,55 @@
-var expect = rquire('expect'),
+var expect = require('expect'),
     TodoAPI = require('TodoAPI');
 
 // start test
 describe('TodoAPI', () => {
+  beforeEach(() => {
+    localStorage.removeItem('todos');
+  });
+
   it('should exist', () => {
     expect(TodoAPI).toExist();
   });
-  //
+  // test if valid data is passed
   describe('setTodos', () => {
+    it('should set valid todos array', () => {
+      var todos = [{
+        id: 23,
+        text: 'test all files',
+        completed: false
+      }];
+      TodoAPI.setTodos(todos);
 
+      var actualTodos = JSON.parse(localStorage.getItem('todos'));
+
+      expect(actualTodos).toEqual(todos);
+    });
+
+    it('should not set invalid todos array,', () => {
+      var badTodos = {a: 'b'};
+      TodoAPI.setTodos(badTodos);
+
+      expect(localStorage.getItem('todos')).toBe(null);
+    });
   });
   //
   describe('getTodos', () => {
+    // test if invalid data is passed
+    it('should return empty array for bad localStorage data', () => {
+      var actualTodos = TodoAPI.getTodos();
+      expect(actualTodos).toEqual([]);
+    });
 
+    if('should return todo if valid array in localStorage', () => {
+      var todos = [{
+        id: 23,
+        text: 'test all files',
+        completed: false
+      }];
+      localStorage.setItem('todos', JSON.stringify(todos));
+      var actualTodos = TodoAPI.getTodos();
+
+      expect(actualTodos).toEqual(todos);
+    });
   });
 });
