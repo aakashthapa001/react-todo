dea90db07369731ad6221eff0e046c86045711c1
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2016-12-07 15:31:17 +0545

    Setup test suite for localStorage

---
+++ app/tests/api/TodoAPI.test.jsx
@@ -0,0 +1,17 @@
+var expect = rquire('expect'),
+    TodoAPI = require('TodoAPI');
+
+// start test
+describe('TodoAPI', () => {
+  it('should exist', () => {
+    expect(TodoAPI).toExist();
+  });
+  //
+  describe('setTodos', () => {
+
+  });
+  //
+  describe('getTodos', () => {
+
+  });
+});
