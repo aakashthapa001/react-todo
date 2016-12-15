edb39eb3ca16d21aa86d64fd4f8270676126eaa9
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2016-12-15 14:37:29 +0545

    Search text to lowercase fixed

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -5,7 +5,7 @@ var uuid = require('node-uuid'),
 export var searchTextReducer = (state = '', action) => {
   switch(action.type) {
     case 'SET_SEARCH_TEXT':
-      return action.searchText;
+      return action.searchText.toLowerCase();
     default:
       return state;
   }
